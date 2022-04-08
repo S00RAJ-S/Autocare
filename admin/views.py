eca68f611cbe287d9b7a7c313a86feebd8847750
SOORAJ <soorajsprabhu15@gmail.com>
2022-04-08 21:56:21 +0530

    admin home page

--- admin/views.py
+++ admin/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import redirect, render
+from login.models import login
 
 def index(request):
+    tu = login.objects.filter(type='u').count()
+    tp = login.objects.filter(type='p').count()
+    tpa = login.objects.filter(status='approved',type='p').count()
+    tpna = login.objects.filter(status='pending',type='p').count()
+    dat ={'tu':tu,'tp':tp,'tpa':tpa,'tpna':tpna}
     try:
         if request.session['e'] and request.session['p'] != '':
             if request.session['t'] ==  'a':
-                return render(request,'adminindex.html')
+                return render(request,'adminindex.html',dat)
             else:
                 return redirect('/')
     except: 
